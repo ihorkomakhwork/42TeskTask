4d0705a1377c196c71cd6e80942e36252ec97ce9
ihorkomakhwork <ihorkomakhwork@gmail.com>
2023-01-03 14:42:15 +0200

    add cert

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,14 @@ module.exports = {
           script: "/usr/local/bin/node-red",
           watch: true,
           env: {
-              "PORT": 3000,
-              "NODE_ENV": "development"
-          }
+            "VISA_AUTH_ID": "XUODRN89PMITTDKCWHTJ21M862xJYdnMaV2B2ivXUZeGEB7bM",
+            "VISA_AUTH_PASSWORD": "7S8ZTHJcr4B1QH",
+            "VISA_HOST": "https://sandbox.api.visa.com/",
+            "VISA_AUTH_PRIVATE_KEY": "/home/komigor/visa/privateKey.pem",
+            "VISA_AUTH_CERT": "/home/komigor/visa/cert.pem",
+            "PORT": 1860,
+            "NODE_ENV": "development"
+            }
         }
     ]
   }
