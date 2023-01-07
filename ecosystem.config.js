5c4857749ddc53722b38ad70052a74c80bab9862
ihorkomakhwork <ihorkomakhwork@gmail.com>
2023-01-07 18:32:45 +0200

    edit config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
           script: "/usr/local/bin/node-red",
           watch: true,
           env: {
+            "VISA_MLE_KEY": "eca211bd-ecb8-4f65-9b5c-80cec9abd5bd", 
+            "VISA_MLE_SERVER_CERT": "/home/komigor/visa/server_cert_MLE.pem",              
             "VISA_AUTH_ID": "XUODRN89PMITTDKCWHTJ21M862xJYdnMaV2B2ivXUZeGEB7bM",
             "VISA_AUTH_PASSWORD": "7S8ZTHJcr4B1QH",
             "VISA_HOST": "https://sandbox.api.visa.com/",
